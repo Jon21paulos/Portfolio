a444c200a1549d0ba5ba873f04e31cd326d7bfc3
Jon21paulos <yohannespaulos1616@gmail.com>
2021-06-25 08:42:12 +0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <Projects />
+      <About />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "../project1.png",
+      link: "#",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "../project1.jpg",
+      link: "#",
+    },
+    
+  ];
+  export const skills = [
+    
+       "MERN Stack","java",".NET FrameWorks","Flutter"
+    ];
+    export const testimonials = [
+        {
+          qoute: "React Reserve",
+          image: "./project1.png",
+          name: "MERN Stack",
+          company:"ALFAD",
+        },];
