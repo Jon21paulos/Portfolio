47885defb9c874c5a93b2608ff4c665198336b70
Jon21paulos <83462298+Jon21paulos@users.noreply.github.com>
2021-06-27 14:50:19 +0300

    error on link tag

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href="#" className="text-indigo-400 leading-relaxed">
                 yohannespaulos1616@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
