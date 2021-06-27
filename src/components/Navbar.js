b3ed4f7be7fb46e130ba5fabb5e833fc95c6d8f7
Jon21paulos <83462298+Jon21paulos@users.noreply.github.com>
2021-06-27 15:00:50 +0300

    e 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+        <div className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Yohannes Paulos
           </a>
-        </a>
+        </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Past Work
